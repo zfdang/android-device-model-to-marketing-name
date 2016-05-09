b29a0bd4c2897d05d6d80e1f1fff2745f549defc
Zhengfa DANG <dantifer@gmail.com>
2016-05-09 08:25:12 +0800

    Initial commit

---
+++ device_model_to_marketing_name/src/main/java/com/zfdang/devicemodeltomarketingname/DeviceMarketingName.java
@@ -0,0 +1,12 @@
+package com.zfdang.devicemodeltomarketingname;
+
+/**
+ * Created by zfdang on 2016-5-9.
+ */
+public class DeviceMarketingName {
+
+    public static String getDeviceMarketingName(){
+        return "";
+    }
+
+}
---
+++ device_model_to_marketing_name/src/main/java/com/zfdang/devicemodeltomarketingname/Mappings.java
@@ -0,0 +1,7 @@
+package com.zfdang.devicemodeltomarketingname;
+
+/**
+ * Created by zfdang on 2016-5-9.
+ */
+public class Mappings {
+}
